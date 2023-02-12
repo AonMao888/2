d162a1e1ff55b4f80c3f264a1d93e90d624cba7d
Aon Mao <aonmao888@gmail.com>
2023-02-12 13:05:16 +0630

    2

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require("express");
+var fs = require("fs");
 var app = express();
 
 app.get("/",(req,res)=>{
     res.send("This is home page")
 })
+
+const db = JSON.parse(fs.readFileSync("data.json","utf-8"));
 app.get("/post",(req,res)=>{
-    res.send("This is post page")
+    res.json(db);
 })
 
 app.listen(80,()=>{
