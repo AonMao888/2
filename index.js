c1519fbf2bce3045c76c715d52ed21e7ad6340cf
Aon Mao <aonmao888@gmail.com>
2023-02-12 21:01:49 +0630

    4

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var express = require("express");
 var fs = require("fs");
 var app = express();
-app.use(express.json())
-app.use(express.static('p'))
+var pass = "memo19";
+app.use(express.json());
 var db = [
-    {"name":"a"},
-    {"name":"b"}
+    
 ];
 
 app.get("/",(req,res)=>{
@@ -13,13 +12,35 @@ app.get("/",(req,res)=>{
 })
 
 app.get("/posts",(req,res)=>{
-    res.json(db);
+    var k = req.query.key;
+    if(k === pass){
+        res.json(db);
+    }else{
+        var fail = {
+            "message":"Something was fail, try again!",
+            "time":Date()
+        };
+        res.json(fail)
+    }
 })
-app.post("/posts",(req,res)=>{
-    var newdata = req.body;
-    console.log(req.body)
-    db.push(newdata);
-    res.json(db);
+app.post("/add/posts",(req,res)=>{
+    var k = req.query.key;
+    if(k === pass){
+        var success = {
+            "message":"Data was successfully added to api",
+            "time":Date()
+        };
+        var newdata = req.body;
+        console.log(req.body)
+        db.push(newdata);
+        res.json(success);
+    }else{
+        var fail = {
+            "message":"Add data was failed, try again!",
+            "time":Date()
+        };
+        res.json(fail)
+    }
 })
 
 app.listen(80,()=>{
