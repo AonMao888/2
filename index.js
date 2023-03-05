ffd7bf7294068641b348ca88a3bc3c2516354aca
Aon Mao <aonmao888@gmail.com>
2023-03-05 10:32:30 +0630

    10

--- index.js
+++ index.js
@@ -1,55 +1,19 @@
-var express = require("express");
-var ejs = require("ejs");
-const e = require("express");
-const ytdl = require("ytdl-core");
-var app = express();
-var pass = "memo19";
-app.use(express.json());
-app.set('views',__dirname+'/views')
-app.set('view engine','ejs');
-var db = [];
+const express = require("express");
+const socket = require("socket.io");
+const app = express();
+app.use(express.static(__dirname+'/p'))
 
-app.get("/",(req,res)=>{
-    res.render('index',{text:"Hello you're"})
-})
-app.get('/d',(req,res)=>{
-    var id = req.query.link.split("v=")[1];
-    res.setHeader('Content-Disposition',`attachment; filename="${id}.mp4"`)
-    ytdl(req.query.link,{quality: 'highest'}).pipe(res)
+app.get('/',(req,res)=>{
+    res.send("hi")
 })
 
-app.get("/posts",(req,res)=>{
-    var k = req.query.key;
-    if(k === pass){
-        res.json(db);
-    }else{
-        var fail = {
-            "message":"Something was fail, try again!",
-            "time":Date()
-        };
-        res.json(fail)
-    }
-})
-app.post("/add/posts",(req,res)=>{
-    var k = req.query.key;
-    if(k === pass){
-        var success = {
-            "message":"Data was successfully added to api",
-            "time":Date()
-        };
-        var newdata = req.body;
-        console.log(req.body)
-        db.push(newdata);
-        res.json(success);
-    }else{
-        var fail = {
-            "message":"Add data was failed, try again!",
-            "time":Date()
-        };
-        res.json(fail)
-    }
+const server = app.listen(80,()=>{
+    console.log("Server started with port 80")
 })
 
-app.listen(80,()=>{
-    console.log("server started with port 80")
+const io = socket(server);
+io.on("connection",(socket)=>{
+    socket.on("h",(data)=>{
+        io.sockets.emit('h',data);
+    })
 })
